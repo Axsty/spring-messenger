6a4983465da0fdd78ad070dea14aaa507c3fb796
Axsty <axelstaaffriborg@gmail.com>
2026-03-31 10:52:46 +0200

    All emails in UPPERCASE!

--- src/main/java/se/iths/axel/springmessenger/service/MessageService.java
+++ src/main/java/se/iths/axel/springmessenger/service/MessageService.java
@@ -3,6 +3,7 @@ package se.iths.axel.springmessenger.service;
 import org.springframework.stereotype.Service;
 import se.iths.axel.springmessenger.messaging.Messenger;
 import se.iths.axel.springmessenger.model.Message;
+import se.iths.axel.textformatterlibrary.TextFormatter;
 
 import java.util.Map;
 
@@ -11,16 +12,18 @@ public class MessageService {
 
     // String i generics hämtar typen messenger, t.ex email för emailSender
     private final Map<String, Messenger> messengers;
+    private final TextFormatter textFormatter;
 
-
-    public MessageService(Map<String, Messenger> messengers) {
+    public MessageService(Map<String, Messenger> messengers, TextFormatter textFormatter) {
         this.messengers = messengers;
+        this.textFormatter = textFormatter;
     }
 
     public void send(Message message) {
         for (String type : messengers.keySet()) {
             if (message.getType().equals(type)) {
                 Messenger messenger = messengers.get(type);
+                message.setMessage(textFormatter.toUpperCase(message.getMessage()));
                 messenger.send(message);
                 return;
             }
